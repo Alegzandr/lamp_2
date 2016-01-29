f64bf1d25f677462cd633cf9275e19215a1a73f4
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-01-29 20:53:32 +0100

    Restarted code

---
+++ cards.php
@@ -0,0 +1,6 @@
+<?php
+
+class Cards
+{
+    public $
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require 'cards.php';
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Blackjack PHP</title>
+</head>
+<body>
+<form method="post">
+    <input type="button" name="draw" value="Tirer une carte">
+    <input type="button" name="surrender" value="Abandonner">
+</form>
+</body>
+</html>
