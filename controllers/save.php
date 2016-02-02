e89a6bd84225258514066305698f4c7014680de4
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-02-02 18:47:00 +0100

    MVC

---
+++ controllers/save.php
@@ -0,0 +1,2 @@
+<?php
+// Enregistre les parties
---
+++ views/blackjack.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Blackjack PHP</title>
+</head>
+<body>
+<fieldset>
+    <legend>Blackjack</legend>
+    <form method="POST">
+        <input type="radio" name="choice" value="yes" checked="checked">
+        <input type="submit" value="ok">
+    </form>
+</fieldset>
+
+<?php
+require_once '../controllers/game.php';
+?>
+</body>
+</html>
