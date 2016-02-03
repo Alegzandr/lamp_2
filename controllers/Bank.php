ce8bb6d47c66e4d570008bc5c72b2006ca1d1ce2
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-02-03 15:12:24 +0100

    Good one

--- controllers/Bank.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Bank extends Player
-{
-    public function __construct()
-    {
-        parent::__construct("Banque");
-    }
-}
---
+++ models/Bank.php
@@ -0,0 +1,10 @@
+<?php
+require_once("Player.php");
+
+class Bank extends Player
+{
+    public function __construct()
+    {
+        parent::__construct("Banque");
+    }
+}
